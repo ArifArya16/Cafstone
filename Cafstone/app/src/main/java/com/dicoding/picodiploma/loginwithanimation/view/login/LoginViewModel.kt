b3b631678e225d381068cef94490f54fe9da9f4d
ArifArya16 <119673022+ArifArya16@users.noreply.github.com>
2024-05-21 20:43:52 +0700

    Add files via upload

---
+++ Cafstone/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/login/LoginViewModel.kt
@@ -0,0 +1,32 @@
+package com.dicoding.picodiploma.loginwithanimation.view.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dicoding.picodiploma.loginwithanimation.data.UserRepository
+import com.dicoding.picodiploma.loginwithanimation.data.response.LoginResponse
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class LoginViewModel(private val repository: UserRepository) : ViewModel() {
+    private val _isStatus = MutableLiveData<LoginResponse?>()
+    val isStatus: LiveData<LoginResponse?> = _isStatus
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    init{
+        _isLoading.value = false
+        _isStatus.value = null
+    }
+
+    fun login(email : String, password : String) {
+        _isLoading.value = true
+        runBlocking {
+            viewModelScope.launch {
+                val a = repository.login(email,password)
+                _isStatus.value = a
+                _isLoading.value = false
+            } }
+    }
+}
---
+++ Cafstone/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/signup/SignUpViewModel.kt
@@ -0,0 +1,32 @@
+package com.dicoding.picodiploma.loginwithanimation.view.signup
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dicoding.picodiploma.loginwithanimation.data.UserRepository
+import com.dicoding.picodiploma.loginwithanimation.data.response.RegisterResponse
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+class SignUpViewModel(private val repository: UserRepository) : ViewModel() {
+    private val _isStatus = MutableLiveData<RegisterResponse?>()
+    val isStatus: LiveData<RegisterResponse?> = _isStatus
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    init{
+        _isStatus.value = null
+        _isLoading.value = false
+    }
+    fun register(nama :String, email : String, password : String){
+        _isLoading.value = true
+        runBlocking {
+            viewModelScope.launch {
+            val a = repository.register(nama,email,password)
+            _isStatus.value = a
+                _isLoading.value = false
+        } }
+    }
+}
